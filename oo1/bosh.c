c5ca609e9eb4b329535d41434d8d527fddc7d430
kirluu <kirluu4thewin@gmail.com>
2015-10-01 12:14:08 +0200

    Fixed Ctrl+C via Christian

--- oo1/bosh.c
+++ oo1/bosh.c
@@ -28,79 +28,68 @@ static volatile int keepRunning = 1;
 
 void intHandler(int dummy)
 {
-  keepRunning = 0;
 }
 
 /* --- use the /proc filesystem to obtain the hostname --- */
 char *gethostname1(char *hostname)
 {
-  FILE *file;
-  file = fopen("/proc/sys/kernel/hostname", "r");
-  fscanf(file, "%s", hostname);
-  fclose(file);
+  int res = gethostname(hostname, HOSTNAMEMAX); // From unistd.h
+  if (res != 0) { // Succeeded
+    return hostname;
+  }
+  hostname = "";
+  return hostname;
 }
 
 /* --- execute a shell command --- */
 int executeshellcmd (Shellcmd *shellcmd)
 {
-  pid_t pid = fork();
-  if (pid == 0) { /* child process*/
-    Cmd *cmdlist = shellcmd->the_cmds;
+  Cmd *cmdlist = shellcmd->the_cmds;
   
-    while (cmdlist != NULL ) {
-      char **cmd = cmdlist->cmd; // Current command
-      cmdlist = cmdlist->next; // Iteration
+  while (cmdlist != NULL ) {
+    char **cmd = cmdlist->cmd; // Current command
+    cmdlist = cmdlist->next; // Iteration
 
-      // Retrieve next command if any
-      char **scndCmd = NULL;
-      if (cmdlist != NULL) // if true: pipe was used
-        scndCmd = cmdlist->cmd; // Next command
+    // Retrieve next command if any
+    char **scndCmd = NULL;
+    if (cmdlist != NULL) // if true: pipe was used
+      scndCmd = cmdlist->cmd; // Next command
 
-      // Control-prints:
-      printf("First command: '%s'\n", cmd[0]);
-      if (scndCmd != NULL)
-        printf("Second command: '%s'\n", scndCmd[0]);
-      
-      printshellcmd(shellcmd);
+    // Control-prints:
+    printf("First command: '%s'\n", cmd[0]);
+    if (scndCmd != NULL)
+      printf("Second command: '%s'\n", scndCmd[0]);
+    
+    printshellcmd(shellcmd);
 
-      // Execution
+    // Execution
 
-      // Store infile and outfile in variables
-      char *rd_stdin = shellcmd->rd_stdin;
-      char *rd_stdout = shellcmd->rd_stdout;
-      // Background vs. Foreground
-      if (shellcmd->background == 1) { // Execute command without waiting for finished execution
-        if (scndCmd != NULL) { // Perform piping
-          pipecmd(*cmd, cmd, *scndCmd, scndCmd, 1); // Performing pipe with current and next command (with their args) in bg
-          if (cmdlist != NULL)
-            cmdlist = cmdlist->next; // Avoid executing 2nd part of pipe (?)
-        }
-        else {
-          backgroundcmd(*cmd, cmd, rd_stdin, rd_stdout);
-        }
+    // Store infile and outfile in variables
+    char *rd_stdin = shellcmd->rd_stdin;
+    char *rd_stdout = shellcmd->rd_stdout;
+    // Background vs. Foreground
+    if (shellcmd->background == 1) { // Execute command without waiting for finished execution
+      if (scndCmd != NULL) { // Perform piping
+        pipecmd(*cmd, cmd, *scndCmd, scndCmd, 1); // Performing pipe with current and next command (with their args) in bg
+        if (cmdlist != NULL)
+          cmdlist = cmdlist->next; // Avoid executing 2nd part of pipe (?)
       }
-      else { // Execute command and wait for it
-        if (scndCmd != NULL) { // Perform piping
-          pipecmd(*cmd, cmd, *scndCmd, scndCmd, 0); // Performing pipe with current and next command (with their args) in fg
-          if (cmdlist != NULL)
-            cmdlist = cmdlist->next; // Avoid executing 2nd part of pipe (?)
-        }
-        else {
-          foregroundcmd(*cmd, cmd, rd_stdin, rd_stdout);
-        }
+      else {
+        backgroundcmd(*cmd, cmd, rd_stdin, rd_stdout);
       }
-      
     }
-    return 0;
-  }
-  else if (pid > 0) { /* parent process */
-    if (!keepRunning){
-      kill(pid, SIGKILL);
+    else { // Execute command and wait for it
+      if (scndCmd != NULL) { // Perform piping
+        pipecmd(*cmd, cmd, *scndCmd, scndCmd, 0); // Performing pipe with current and next command (with their args) in fg
+        if (cmdlist != NULL)
+          cmdlist = cmdlist->next; // Avoid executing 2nd part of pipe (?)
+      }
+      else {
+        foregroundcmd(*cmd, cmd, rd_stdin, rd_stdout);
+      }
     }
   }
-  else {
-    printf("Failed fork in executeshellcmd.");
-  }
+  return 0;
 }
 
 /* --- main loop of the simple shell --- */
@@ -111,22 +100,22 @@ int main(int argc, char* argv[]) {
   char hostname[HOSTNAMEMAX];
   int terminate = 0;
   Shellcmd shellcmd;
-  
-  if (!gethostname1(hostname)) {
 
-    signal(SIGINT, intHandler); // Listen for Ctrl + C
+  signal(SIGINT, intHandler); // Listen for Ctrl + C
+  
+  if (gethostname1(hostname)) {
 
     /* parse commands until exit or ctrl-c */
     while (!terminate) {
       printf("%s", hostname);
       if (cmdline = readline(":# ")) {
       	if(*cmdline) {
+          if (checkIfExit(cmdline)) {
+            return EXIT_SUCCESS;
+          }
       	  add_history(cmdline);
       	  if (parsecommand(cmdline, &shellcmd)) {
-            if (checkIfExit(cmdline)) {
-              return EXIT_SUCCESS;
-            }
-      	    terminate = executeshellcmd(&shellcmd);
+      	    executeshellcmd(&shellcmd);
       	  }
       	}
       	free(cmdline);
