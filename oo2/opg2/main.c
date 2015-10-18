35e61805d2b1371340bd3ed6776433a22f90fb6f
kirkbro <maek@itu.dk>
2015-10-18 14:26:44 +0200

    Updated opg2

--- oo2/opg2/main.c
+++ oo2/opg2/main.c
@@ -17,57 +17,22 @@ int main(int argc, char* argv[])
 {
   fifo = list_new();
 
-  Node *n1;
-  Node *n2;
-  Node *n3;
+  list_add(fifo, node_new_str("s1"));
+  list_add(fifo, node_new_str("s2"));
 
-  printf("Let us begin...\n");
-  int pid = fork();
-  if (pid == 0) // Child process: Add 2, read one
-  {
-    list_add(fifo, node_new_str("s14"));
+  list_remove(fifo);
+  list_remove(fifo);
+  list_remove(fifo);
+  
+  list_add(fifo, node_new_str("s1"));
+  list_add(fifo, node_new_str("s2"));
 
-    n2 = list_remove(fifo);
-    if (n2 == NULL) { printf("Error no elements in list\n"); }
-    
-    list_add(fifo, node_new_str("s34"));
-    
-    // Print results
-    if (n2 != NULL) {
-      printf("%s\n", (char*)n2->elm);
-    }
-    else{
-      printf("n3 ded.\n");
-    }
-  }
-  else // Parent process: Read two, add one
-  {
-    n1 = list_remove(fifo);
-    if (n1 == NULL) { printf("Error no elements in list\n"); }
-    
-    list_add(fifo, node_new_str("s24"));
-    
-    n3 = list_remove(fifo);
-    if (n3 == NULL) { printf("Error no elements in list\n"); }
-    
-    // Print results
-    if (n1 != NULL) {
-      printf("%s\n", (char*)n1->elm);
-    }
-    else{
-      printf("n1 ded.\n");
-    }
-    if (n3 != NULL){
-      printf("%s\n", (char*)n3->elm);
-    }
-    else{
-      printf("n3 ded.\n");
-    }
-    
-    
-    // Wait for child
-    waitpid(pid, NULL, 0);
-  }
+  Node *n1 = list_remove(fifo);
+  if (n1 == NULL) { printf("Error no elements in list\n"); exit(-1);}
+  Node *n2 = list_remove(fifo);
+  if (n2 == NULL) { printf("Error no elements in list\n"); exit(-1);}
+  printf("%s\n%s\n", (char *) n1->elm, (char *) n2->elm);
+
   return 0;
 }
 
